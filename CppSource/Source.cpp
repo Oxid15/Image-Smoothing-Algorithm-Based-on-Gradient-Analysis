60a6fe574df6828ab2bf51e9f41499102abb0082
Ilya <villeman.5@yandex.ru>
2020-09-06 22:52:08 +0500

    Added new interface for opencv data types for C++ version

--- CppSource/Source.cpp
+++ CppSource/Source.cpp
@@ -13,6 +13,8 @@ in choosing the data types that are recommended to use (in parenthesis):
 #include "stdint.h"
 #include <cmath>
 
+using namespace cv;
+
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
 
 template<typename Tf, typename Ts>
@@ -106,7 +108,7 @@ public:
 		for (uint32_t c = 0; c < colors; c++)
 			for (uint32_t i = 0; i < height; i++)
 				for (uint32_t j = 0; j < width; j++)
-				{		
+				{
 					int up = i - ksize / 2;
 					int left = j - ksize / 2;
 					int down = i + ksize / 2 + 1;
@@ -143,4 +145,119 @@ public:
 						dst[i][j][c] = Tf(src[i][j][c]);
 				}
 	}
-}; 
+
+	/*
+		Easy to use interface for filtering using opencv Mat data type.
+		Recieves:
+			Mat src - image to be filtered
+			uint32_t ksize - kernel size
+		Returns:
+			Mat - filtered image
+		Usage example:
+			Mat input = imread(<input path>, IMREAD_COLOR);
+
+			Filter<float, uint8_t> filter;
+			Mat output = filter(input, <kernel size>);
+
+			imwrite(<output path>, output);
+	*/
+	Mat operator()(Mat src, uint32_t ksize)
+	{
+		//allocating memory
+		uint32_t height = src.rows;
+		uint32_t width = src.cols;
+		uint32_t colors = src.channels();
+
+		uint8_t*** image = new uint8_t**[height];
+		float*** output = new float**[height];
+		uint8_t*** intoutput = new uint8_t**[height];
+		float*** modules = new float**[height];
+		float*** angles = new float**[height];
+		int32_t**** grads = new int32_t***[height];
+		for (uint32_t i = 0; i < height; i++)
+		{
+			image[i] = new uint8_t*[width];
+			output[i] = new float*[width];
+			intoutput[i] = new uint8_t*[width];
+			modules[i] = new float*[width];
+			angles[i] = new float*[width];
+			grads[i] = new int32_t**[width];
+			for (uint32_t j = 0; j < width; j++)
+			{
+				image[i][j] = src.ptr<uint8_t>(i, j);
+
+				output[i][j] = new float[colors];
+				intoutput[i][j] = new uint8_t[colors];
+				modules[i][j] = new float[colors];
+				angles[i][j] = new float[colors];
+				grads[i][j] = new int32_t*[colors];
+				for (uint32_t c = 0; c < colors; c++)
+				{
+					output[i][j][c] = .0;
+					intoutput[i][j][c] = 0;
+					modules[i][j][c] = .0;
+					angles[i][j][c] = .0;
+					grads[i][j][c] = new int32_t[2];
+					for (uint32_t k = 0; k < 2; k++)
+						grads[i][j][c][k] = 0;
+				}
+			}
+		}
+
+		computeGrads<uint8_t, int32_t>(image, grads, height, width, colors);
+		computeModules<float, uint8_t, int32_t>(image, modules, grads, height, width, colors);
+		computeAngles<float, uint8_t, int32_t>(image, angles, grads, height, width, colors);
+
+		//filtering with regular function
+		this->operator()(image, output, modules, angles, ksize, height, width, colors);
+		//
+
+		for (uint32_t i = 0; i < height; i++)
+		{
+			for (uint32_t j = 0; j < width; j++)
+			{
+				for (uint32_t c = 0; c < colors; c++)
+					delete grads[i][j][c];
+				delete grads[i][j];
+				delete modules[i][j];
+				delete angles[i][j];
+			}
+			delete modules[i];
+			delete angles[i];
+			delete grads[i];
+		}
+		delete modules;
+		delete angles;
+		delete grads;
+
+		//converting to integer, flattening and returning new Mat image
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				for (int c = 0; c < colors; c++)
+					intoutput[i][j][c] = (uint8_t)MIN(output[i][j][c], 255);
+				delete output[i][j];
+			}
+			delete output[i];
+		}
+		delete output;
+
+		Mat matOutput = Mat::zeros(height, width, CV_8UC3);
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				for (int c = 0; c < colors; c++)
+				{
+					matOutput.ptr<uint8_t>(i, j)[c] = intoutput[i][j][c];
+				}
+				delete intoutput[i][j];
+			}
+			delete intoutput[i];
+		}
+		delete intoutput;
+
+		return matOutput;
+	}
+};
