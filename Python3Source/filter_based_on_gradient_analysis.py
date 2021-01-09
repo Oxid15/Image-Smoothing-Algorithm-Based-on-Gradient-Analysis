3829fc4e94df4c1b0edce371f37c4de8120b5571
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2021-01-09 17:52:25 +0500

    Python implementation refactoring and iterative mode added

--- Python3Source/filter_based_on_gradient_analysis.py
+++ Python3Source/filter_based_on_gradient_analysis.py
@@ -6,120 +6,190 @@ import numpy as np
 from math import sqrt, atan2, cos, isnan
 
 
-def euclid_norm(vect): 
+def _euclid_norm(vect):
     return sqrt(vect[0]*vect[0] + vect[1]*vect[1])
 
 
-def angle_rad(vect):
+def _angle_rad(vect):
     return atan2(vect[1], vect[0])
 
 
-def grad(x, y, image):
+def _grad(x, y, image):
     gradx = image[y][x-1] - image[y][x+1]
     grady = image[y+1][x] - image[y-1][x] 
-    return np.array([gradx, grady])
+    return [gradx, grady]
 
 
-def compute_grads(image):
-    grads = np.zeros((image.shape[0], image.shape[1], 2))
+def compute_grads_channel(image, grads):
     for y in range(image.shape[0]):
         for x in range(image.shape[1]):
             if 0 < x < image.shape[1] - 1 and 0 < y < image.shape[0] - 1:
-                grads[y, x] = grad(x, y, image)
-    return grads
+                grads[y, x] = _grad(x, y, image)
 
 
-def compute_modules(image, grads=None):
-    newimage = np.zeros(image.shape)
+def compute_grads(image, grads):
+    compute_grads_channel(image[:, :, 0], grads[:, :, :, 0])
+    compute_grads_channel(image[:, :, 1], grads[:, :, :, 1])
+    compute_grads_channel(image[:, :, 2], grads[:, :, :, 2])
+
+
+def compute_modules_channel(image, modules, grads):
+    for y in range(image.shape[0]):
+        for x in range(image.shape[1]):
+            if 0 < x < image.shape[1] - 1 and 0 < y < image.shape[0] - 1:
+                modules[y][x] = _euclid_norm(grads[y][x])
+
+
+def compute_modules(image, modules, grads):
+    compute_modules_channel(image[:, :, 0], modules[:, :, 0], grads[:, :, :, 0])
+    compute_modules_channel(image[:, :, 1], modules[:, :, 1], grads[:, :, :, 1])
+    compute_modules_channel(image[:, :, 2], modules[:, :, 2], grads[:, :, :, 2])
+
+
+def compute_angles_channel(image, angles, grads):
+    for y in range(image.shape[0]):
+        for x in range(image.shape[1]):
+            if 0 < x < image.shape[1] - 1 and 0 < y < image.shape[0] - 1:
+                angle = _angle_rad(grads[y, x])
+                if not isnan(angle):
+                    angles[y, x] = angle
+                else:
+                    angles[y, x] = 0
+
+
+def compute_angles(image, angles, grads):
+    compute_angles_channel(image[:, :, 0], angles[:, :, 0], grads[:, :, :, 0])
+    compute_angles_channel(image[:, :, 1], angles[:, :, 1], grads[:, :, :, 1])
+    compute_angles_channel(image[:, :, 2], angles[:, :, 2], grads[:, :, :, 2])
+
+
+def smooth_channel(src, k_size, n=1, grads=None, modules=None, angles=None, dst=None):
+    """
+    Smooth 1-channel image with filter based on gradient analysis
+
+    :param src: source image with shape (n, m, 3)
+    :param k_size: kernel size - odd integer
+    :param n: the number of sequential runs
+    :param grads: gradient vectors for each pixel with shape (n, m, 2, 3)
+    :param modules: gradient modules for each pixel with shape (n, m, 3)
+    :param angles: gradient angles for each pixel with shape (n, m, 3)
+    :param dst: destination image with the same shape of type np.float64
+    :return: only if dst is not None: smoothed image with same shape as src and type np.float64
+    """
+
+    src_proxy = np.copy(src)
+    if dst is None:
+        dst = np.zeros(src.shape, np.float64)
+    for i in range(n):
+        if i == 0:
+            _smooth_channel(src_proxy, k_size, grads=grads, modules=modules, angles=angles, dst=dst)
+        else:
+            _smooth_channel(src_proxy, k_size, dst=dst)
+        src_proxy = dst
+    if dst is not None:
+        return dst
+
+
+def _smooth_channel(src, k_size, grads=None, modules=None, angles=None, dst=None):
+    if dst is None:
+        dst = np.zeros(src.shape, dtype=np.float64)
     if grads is None:
-        for y in range(image.shape[0]):
-            for x in range(image.shape[1]):
-                if 0 < x < image.shape[1] - 1 and 0 < y < image.shape[0] - 1:
-                    newimage[y][x] = euclid_norm(grad(x,y,image))
-    else:
-        for y in range(image.shape[0]):
-            for x in range(image.shape[1]):
-                if 0 < x < image.shape[1] - 1 and 0 < y < image.shape[0] - 1:
-                    newimage[y][x] = euclid_norm(grads[y][x])
-    return newimage
+        grads = np.zeros((src.shape[0], src.shape[1], 2))
+        compute_grads_channel(src.astype(np.float64), grads)
+        if modules is None:
+            modules = np.zeros((src.shape[0], src.shape[1]))
+            compute_modules_channel(src.astype(np.float64), modules, grads)
+        if angles is None:
+            angles = np.zeros((src.shape[0], src.shape[1]))
+            compute_angles_channel(src.astype(np.float64), angles, grads)
 
-
-def compute_angles(image, grads=None):
-    newimage = np.zeros(image.shape)
-    if grads is None:
-        for y in range(image.shape[0]):
-            for x in range(image.shape[1]):
-                if 0 < x < image.shape[1] - 1 and 0 < y < image.shape[0] - 1:
-                    angle = angle_rad(grad(x, y, image))
-                    if not isnan(angle):
-                        newimage[y][x] = angle
-                    else:
-                        newimage[y][x] = 0
-    else:
-        for y in range(image.shape[0]):
-            for x in range(image.shape[1]):
-                if 0 < x < image.shape[1] - 1 and 0 < y < image.shape[0] - 1:
-                    angle = angle_rad(grad(x, y, image))
-                    if not isnan(angle):
-                        newimage[y][x] = angle
-                    else:
-                        newimage[y][x] = 0
-    return newimage
-
-
-def filter_channel(src, dst, modules, angles, ksize):
     for i in range(src.shape[0]):
         for j in range(src.shape[1]):
-            up = i - ksize // 2
-            left = j - ksize // 2
-            down = i + ksize // 2 + 1
-            right = j + ksize // 2 + 1
+            up = i - k_size // 2
+            left = j - k_size // 2
+            down = i + k_size // 2 + 1
+            right = j + k_size // 2 + 1
             sum_weights = 0
             result = 0
-            for k in range(up, down):
-                if k < 0 or k >= src.shape[0]:
+            for s in range(up, down):
+                if s < 0 or s >= src.shape[0]:
                     continue
-                for l in range(left, right):
-                    if l < 0 or l >= src.shape[1]:
+                for t in range(left, right):
+                    if t < 0 or t >= src.shape[1]:
                         continue
-                    if modules[k][l] == .0:
+                    if modules[s][t] == .0:
                         continue
-                    weight = .0
-                    if k != i or l != j:
-                        alpha = 1. / modules[k][l]
-                        beta = 2. * (angles[i][j] - angles[k][l])
+
+                    if s != i or t != j:
+                        alpha = 1. / modules[s][t]
+                        beta = 2. * (angles[i][j] - angles[s][t])
                         weight = (cos(beta) + 1) * alpha
                     else:
                         # weight of central pixel
                         weight = 1.
-                    result += weight * src[k][l]
+                    result += weight * src[s][t]
                     sum_weights += weight
             if sum_weights != 0:
                 dst[i][j] = round(result / sum_weights)
             else:
                 # pixel remains without changes if sum of weights = 0
                 dst[i][j] = src[i][j]
+    if dst is not None:
+        return dst
 
 
-def filter_(src, ksize, grads=None, modules=None, angles=None):
-    if len(src.shape) == 3:
-        red, green, blue = src[:, :, 0].astype(np.float32), \
-                           src[:, :, 1].astype(np.float32), \
-                           src[:, :, 2].astype(np.float32)
-        red = filter_(red,   ksize)
-        green = filter_(green, ksize)
-        blue = filter_(blue,  ksize)
-
-        result = np.dstack((red, green, blue))
-        return result
-
-    newimage = np.zeros(src.shape)
+def _smooth(src, dst, k_size, grads=None, modules=None, angles=None):
     if grads is None:
-        grads = compute_grads(src)
+        grads = np.zeros((src.shape[0], src.shape[1], 2, 3))
+        compute_grads(src.astype(np.float64), grads)
     if modules is None:
-        modules =  compute_modules(src, grads=grads)
+        modules = np.zeros((src.shape[0], src.shape[1], 3))
+        compute_modules(src.astype(np.float64), modules, grads)
     if angles is None:
-        angles = compute_angles(src, grads=grads)
+        angles = np.zeros((src.shape[0], src.shape[1], 3))
+        compute_angles(src.astype(np.float64), angles, grads)
 
-    filter_channel(src, newimage, modules, angles, ksize)
-    return newimage
+    red, green, blue = \
+        src[:, :, 0].astype(np.float64), \
+        src[:, :, 1].astype(np.float64), \
+        src[:, :, 2].astype(np.float64)
+
+    smooth_channel(red, k_size,
+                   grads=grads[:, :, :, 0],
+                   modules=modules[:, :, 0],
+                   angles=angles[:, :, 0],
+                   dst=dst[:, :, 0])
+    smooth_channel(green, k_size,
+                   grads=grads[:, :, :, 1],
+                   modules=modules[:, :, 1],
+                   angles=angles[:, :, 1],
+                   dst=dst[:, :, 1])
+    smooth_channel(blue, k_size,
+                   grads=grads[:, :, :, 2],
+                   modules=modules[:, :, 2],
+                   angles=angles[:, :, 2],
+                   dst=dst[:, :, 2])
+    return dst
+
+
+def smooth(src, k_size, n=1, grads=None, modules=None, angles=None):
+    """
+    Smooth 3-channel image with filter based on gradient analysis
+
+    :param src: source image with shape (n, m, 3)
+    :param k_size: kernel size - odd integer
+    :param n: the number of sequential runs
+    :param grads: gradient vectors for each pixel with shape (n, m, 2, 3)
+    :param modules: gradient modules for each pixel with shape (n, m, 3)
+    :param angles: gradient angles for each pixel with shape (n, m, 3)
+    :return: smoothed image with same shape as src and type np.float64
+    """
+    src_proxy = np.copy(src)
+    dst = np.zeros(src.shape, np.float64)
+    for i in range(n):
+        if i == 0:
+            _smooth(src_proxy, dst, k_size, grads=grads, modules=modules, angles=angles)
+        else:
+            _smooth(src_proxy, dst, k_size)
+        src_proxy = dst
+    return dst
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ In this paper image smoothing algorithm based on gradient analysis is proposed. 
 ![boundaries](/images/boundaries.png)  
 Regular boundaries have small deviations of gradient angles and the opposite for irregular ones. To measure closeness of angles cosine of doubled difference is used. As additional measure that helps to discriminate the types of boundaries inverted gradient values were used.  
 ![gradients](/images/gradients.png)  
-When gradient magnitudes are inverted bigger values refer to textures (insignificant changes in gradient) and smaller refer to strong boundaries. So textures would have bigger weights and hence they would appear smoother. We also propose to filter image of gradient magnitudes with median filter to enhance visual quality of results. The method proposed in this paper is easy to implement and compute and it gives good results in comparison with other techniques like bilateral filter.  
+When gradient magnitudes are inverted bigger values refer to textures (insignificant changes in gradient) and smaller refer to strong boundaries. So textures would have bigger weights and hence they would appear smoother. We also propose to smooth image of gradient magnitudes with median smooth to enhance visual quality of results. The method proposed in this paper is easy to implement and compute and it gives good results in comparison with other techniques like bilateral smooth.  
   
 ## Examples
 ![example2](/images/example2.jpg)  
@@ -20,7 +20,7 @@ c) - bilateral filter
 d) - our filter  
 ![comparison](/images/comparison.png)
 ## Edge detection
-Here is the output of Canny edge detector that was applied on the image with and without preprocessing with our filter.
+Here is the output of Canny edge detector that was applied on the image with and without preprocessing with our smooth.
 ![edges](/images/edge_detection.png)
 
 ## How to use code
@@ -39,8 +39,8 @@ int main()
     cv::Mat img = cv::imread("your_input_file_name", cv::IMREAD_COLOR);       //read image using opencv from file into Mat type
     
     int kernelSize = 3;                                                       //set kernelSize = 3 for filtering with 3x3 kernel
-    Filter<float, uint8_t> filter;                                            //create the instance of filter
-    cv::Mat output = filter(img, kernelSize);                                 //filter image
+    Filter<float, uint8_t> smooth;                                            //create the instance of smooth
+    cv::Mat output = smooth(img, kernelSize);                                 //smooth image
     
     cv::imwrite("your_output_file_name", output);                             //write the result
     return 0;
@@ -48,11 +48,12 @@ int main()
 ```
 Here is example with python:
 ```python
-from Source import*
+from filter_based_on_gradient_analysis import fga
 import cv2
 
-img = cv2.imread('your_input_file_name', cv2.IMREAD_COLOR) #read images using opencv from file
-kernel_size = 3                                            #set kernelSize = 3 for filtering with 3x3 kernel
-output = filter_(img, kernel_size)                                  #filter image
-cv2.imwrite('your_output_file_name', output)                  #write the result
+img = cv2.imread('your_input_file_name', cv2.IMREAD_COLOR)  # read images using opencv from file
+kernel_size = 3                                             # set kernel_size = 3 for filtering with 3x3 kernel
+runs_number = 2                                             # set number of runs: parameter n is 1 by default
+output = fga.smooth(img, kernel_size, n=runs_number)        # smooth image
+cv2.imwrite('your_output_file_name', output)                # write the result
 ```
